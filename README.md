469410c1afc1fe5e8480ecf8b7995b6246f73931
Sergey Shelepov <shelepov-sa@yandex.ru>
2025-03-17 13:22:19 +0300

    first review

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Проектная работа Mesto
 
+https://github.com/mp52-wd/mesto-project-ff#
--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,25 @@
-// @todo: Темплейт карточки
+const template = document.querySelector('#card-template')
+const listContainer = document.querySelector('.places__list')
 
-// @todo: DOM узлы
+initialCards.forEach(item => {
+	listContainer.append(generateCard(item, removeCard))
+})
 
-// @todo: Функция создания карточки
+function generateCard(data, removeHandler) {
+	const cardElement = document.importNode(template.content, true).querySelector('.card')
+	const imageElement = cardElement.querySelector('.card__image')
+	const titleElement = cardElement.querySelector('.card__title')
+	const deleteBtn = cardElement.querySelector('.card__delete-button')
 
-// @todo: Функция удаления карточки
+	imageElement.src = data.link
+	imageElement.alt = `Изображение: ${data.name}`
+	titleElement.textContent = data.name
 
-// @todo: Вывести карточки на страницу
+	deleteBtn.addEventListener('click', () => removeHandler(cardElement))
+
+	return cardElement
+}
+
+function removeCard(card) {
+	card.remove()
+}
